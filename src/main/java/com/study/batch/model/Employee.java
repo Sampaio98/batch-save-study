0b88ecfa2098e527a9b4af0bce9df1e69ce0b06c
Allisson <allissonsampaio98@gmail.com>
2021-01-17 03:45:43 -0300

    Adicionado atributo transiente

--- src/main/java/com/study/batch/model/Employee.java
+++ src/main/java/com/study/batch/model/Employee.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -39,6 +40,7 @@ public class Employee implements Persistable<UUID> {
     @UpdateTimestamp
     private LocalDateTime updateDate;
 
+    @Transient
     private boolean isNew;
 
 }
